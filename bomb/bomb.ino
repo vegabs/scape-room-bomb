1a40881206e46e21e750a39c6058526428a82039
gvega <gabrielavl.31@hotmail.com>
2021-08-24 00:35:03 -0500

    todo menos contra

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -10,9 +10,14 @@ void bomb() {
 
   static uint8_t estadoBomba = 0; // 0: desarmada, 1: armada
 
-  static const uint8_t intervalo = 1000;
-  static uint8_t previousMillis = 0;
+  static const uint32_t interval = 1000;
+  static const uint32_t interval2 = 100;
+  static uint32_t previousMillis = 0;
+  static uint32_t previousMillis2 = 0;
   static uint8_t cuentaBomba = 20;
+  static uint8_t cuentaTeclado = 0;
+  static uint8_t contraTeclado[6];
+  static const uint8_t contraSegura[] = {2, 1, 2, 2, 1, 3};
 
   if (estadoBomba == 0) {
 
@@ -42,9 +47,42 @@ void bomb() {
   }
 
   if (estadoBomba == 1) {
-    cuentaBomba--;
-    Serial.print("Quedan: ");
-    Serial.println(cuentaBomba);
+    uint32_t currentMillis = millis();
+
+    if ( (currentMillis - previousMillis) >= interval ) {
+      
+      previousMillis = currentMillis;
+      cuentaBomba--;
+      Serial.print("Quedan: ");
+      Serial.println(cuentaBomba);
+    }
+    else if ((currentMillis - previousMillis2) >= interval2) {
+      
+      previousMillis2 = currentMillis;
+      
+      // verificar codigo de seguridad
+      if (digitalRead(upPin) == HIGH) {
+        Serial.println("estoy aqui");
+        //contraTeclado[cuentaTeclado] = 1;
+        //cuentaTeclado++;
+      }
+      if (digitalRead(downPin) == HIGH) {
+        contraTeclado[cuentaTeclado] = 2;
+        cuentaTeclado++;
+      }
+      if (digitalRead(armPin) == HIGH) {
+        contraTeclado[cuentaTeclado] = 3;
+        cuentaTeclado++;
+
+        if (cuentaTeclado == 6) {
+          if (contraTeclado == contraSegura) {
+            Serial.println("Bomba desarmada");
+            estadoBomba = 0;
+            memset(contraTeclado, 0, sizeof(contraTeclado));
+          }
+        }
+      }
+    }
 
     if (cuentaBomba == 0) {
       Serial.println("BOMBA EXPLOTO");
@@ -52,9 +90,12 @@ void bomb() {
       estadoBomba = 0;
     }
   }
+}
 
+void senal() {
 }
 
 void loop() {
   bomb();
+  senal();
 }
