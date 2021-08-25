f1f7034762febf7a855aeef539e9b0d6fe836fc7
gvega <gabrielavl.31@hotmail.com>
2021-08-24 21:32:19 -0500

    pdf

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -1,31 +1,32 @@
 void setup() {
   Serial.begin(9600);
+  pinMode(3, OUTPUT);
 }
 
 bool verificaClave(uint8_t *pclaveUser, uint8_t *pclaveCorrecta, uint8_t sizeContra) {
-
-  bool resu = 0;
-  uint8_t correctas = 0;
+  bool arregloIgual = 0;
+  uint8_t charIgual = 0;
   for (int i = 0; i < sizeContra; i++) {
-    if (*(pclaveUser + i) == *(pclaveCorrecta + i)) correctas++;
+    if (*(pclaveUser + i) == *(pclaveCorrecta + i)) charIgual++;
   }
 
-  (correctas == 6) ? Serial.println("BOMBA DESARMADA") : Serial.println("INCORRECTO");
-  (correctas == 6) ? resu = 1 : resu = 0;
-  return resu;
+  (charIgual == 6) ? Serial.println("BOMBA DESARMADA") : Serial.println("INCORRECTO");
+  (charIgual == 6) ? arregloIgual = 1 : arregloIgual = 0;
+  return arregloIgual;
 }
 
 void bomb() {
   static const uint8_t upPin = 2;
-  static const uint8_t downPin = 3;
-  static const uint8_t armPin = 4;
+  static const uint8_t downPin = 4;
+  static const uint8_t armPin = 5;
 
   static uint8_t cuentaUpdate = 0;
   static uint8_t estadoBomba = 0; // 0: desarmada, 1: armada
   static const uint32_t interval = 1000;
-  static const uint32_t interval2 = 50;
+  static const uint32_t interval2 = 80;
   static uint32_t previousMillis = 0;
   static uint32_t previousMillis2 = 0;
+  static uint32_t previousMillisSET = 0;
   static uint8_t cuentaBomba = 20;
   static uint8_t cuentaTeclado = 0;
   static uint8_t contraTeclado[6];
@@ -33,39 +34,35 @@ void bomb() {
 
   // estado de configuracion
   if (estadoBomba == 0) {
-
-    if (cuentaBomba >= 10 && cuentaBomba <= 60) {
-
-      if (digitalRead(upPin) == HIGH) cuentaBomba++;
-      if (digitalRead(downPin) == HIGH) cuentaBomba--;
-
-      if (cuentaBomba == 9) cuentaBomba = 10;
-      if (cuentaBomba == 61) cuentaBomba = 60;
+    uint32_t currentMillisSET = millis();
+    if ((currentMillisSET - previousMillisSET) >= interval2 ) {
+      previousMillisSET = currentMillisSET;
+      if (cuentaBomba >= 10 && cuentaBomba <= 60) {
+        if (digitalRead(upPin) == HIGH) cuentaBomba++;
+        if (digitalRead(downPin) == HIGH) cuentaBomba--;
+        if (cuentaBomba == 9) cuentaBomba = 10;
+        if (cuentaBomba == 61) cuentaBomba = 60;
+      }
+      Serial.println("SET: 00:" + (String)cuentaBomba);
     }
 
-    Serial.println("SET: 00:" + (String)cuentaBomba);
 
     if (digitalRead(armPin) == HIGH) {
       Serial.println("BOMBA ARMADA");
       estadoBomba = 1;
     }
-
   }
 
-  // estado conteo
+  // estado bomba armada
   if (estadoBomba == 1) {
-
     uint32_t currentMillis = millis();
-
     if ((currentMillis - previousMillis) >= interval ) {
       previousMillis = currentMillis;
       Serial.println("TIMER: 00:" + (String)cuentaBomba);
       cuentaBomba--;
     }
-
     else if ((currentMillis - previousMillis2) >= interval2) {
       previousMillis2 = currentMillis;
-
       // verificar codigo de seguridad
       if (digitalRead(upPin) == HIGH) {
         Serial.println("UP");
@@ -82,26 +79,21 @@ void bomb() {
         contraTeclado[cuentaTeclado] = 3;
         cuentaTeclado++;
       }
-
+      // verificar que llegaron 6 pulsaciones
       if (cuentaTeclado == 6) {
         bool esClave = verificaClave(contraTeclado, contraSegura, cuentaTeclado);
         if (esClave == true) {
-          cuentaBomba = 20;
-          estadoBomba = 0;
-          cuentaUpdate = 0;
+          cuentaBomba = 20; estadoBomba = 0; cuentaUpdate = 0;
           delay(100);
         }
         cuentaTeclado = 0;
       }
-
       cuentaUpdate++;
     }
 
     if (cuentaBomba == 0) {
       Serial.println("BOMBA EXPLOTO");
-      cuentaBomba = 20;
-      cuentaUpdate = 0;
-      estadoBomba = 0;
+      cuentaBomba = 20; cuentaUpdate = 0; estadoBomba = 0;
     }
   }
 }
@@ -114,11 +106,11 @@ void senal() {
 
   if (currentMillisPWM - previousMillisPWM >= intervalPWM) {
     previousMillisPWM = currentMillisPWM;
- 
+
     if (PWMState == LOW) {
       PWMState = HIGH;
       digitalWrite(8, 1);
-      
+
     } else {
       PWMState = LOW;
       digitalWrite(8, 0);
@@ -126,7 +118,7 @@ void senal() {
   }
 }
 
-  void loop() {
-    bomb();
-    senal();
-  }
+void loop() {
+  bomb();
+  senal();
+}
